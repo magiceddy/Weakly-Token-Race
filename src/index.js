ffe80fad6d00196604483805dd37dd5a40a60b89
magiceddy <rinaldo.rossi.web@gmail.com>
2018-08-28 14:20:54 +0200

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from './App';
+import style from './main';
+
+const test = (a, b) => 'c';
+test(style, App);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.css']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    open: true,
+    host: '0.0.0.0',
+    port: 8080
+  }
+};
